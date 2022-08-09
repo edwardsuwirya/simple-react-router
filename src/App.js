cd7ad1d256610c943d3541574e2f15f8eea29c9e
edwardsuwirya <edward_suwirya@yahoo.com>
2022-08-09 08:17:00 +0700

    Move route definition to App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@
 // 1. install dependency : npm install react-router-dom@6
 // 2. mengkoneksikan aplikasi kita ke browser URL (index.js)
 import Navigation from "./navigation/Navigation";
+import {Route, Routes} from "react-router-dom";
+import LoginPageView from "./features/login/LoginPageView";
+import ProtectedRoute from "./navigation/ProtectedRoute";
+import MasterPageView from "./MasterPageView";
+import BillPageView from "./features/bill/BillPageView";
+import BillInfoView from "./features/bill/BillInfoView";
+import {AuthProvider} from "./shared/hook/useAuth";
+import React from "react";
 
 /*
 rem = “root em”, unit of measurement font size dari sebuah root element.
@@ -16,10 +24,36 @@ font-size: 0.75rem; //0.75 dari 16px = 12px
  */
 function App() {
     return (
-        <div>
+        <AuthProvider>
             <h1>WMB Point Of Sales</h1>
-            <Navigation/>
-        </div>
+            <Routes>
+                <Route index element={<LoginPageView/>}/>
+                <Route element={<ProtectedRoute/>}>
+                    <Route path="main" element={<Navigation/>}>
+                        <Route path="master" element={<MasterPageView/>}/>
+                        <Route path="bill" element={<BillPageView/>}>
+                            <Route
+                                index
+                                element={
+                                    <main style={{padding: "1rem"}}>
+                                        <p>Select an invoice</p>
+                                    </main>
+                                }
+                            />
+                            <Route path=":billId" element={<BillInfoView/>}/>
+                        </Route>
+                    </Route>
+                </Route>
+                <Route
+                    path="*"
+                    element={
+                        <main style={{padding: "1rem"}}>
+                            <p>Oopss... page not found</p>
+                        </main>
+                    }
+                />
+            </Routes>
+        </AuthProvider>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,48 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import MasterPageView from "./MasterPageView";
-import BillPageView from "./features/bill/BillPageView";
-import BillInfoView from "./features/bill/BillInfoView";
-import LoginPageView from "./features/login/LoginPageView";
-import ProtectedRoute from "./navigation/ProtectedRoute";
-import {AuthProvider} from "./shared/hook/useAuth";
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-
         <BrowserRouter>
-            <AuthProvider>
-                <Routes>
-                    <Route index element={<LoginPageView/>}/>
-                    <Route element={<ProtectedRoute/>}>
-                        <Route path="main" element={<App/>}>
-                            <Route path="master" element={<MasterPageView/>}/>
-                            <Route path="bill" element={<BillPageView/>}>
-                                <Route
-                                    index
-                                    element={
-                                        <main style={{padding: "1rem"}}>
-                                            <p>Select an invoice</p>
-                                        </main>
-                                    }
-                                />
-                                <Route path=":billId" element={<BillInfoView/>}/>
-                            </Route>
-                        </Route>
-                    </Route>
-                    <Route
-                        path="*"
-                        element={
-                            <main style={{padding: "1rem"}}>
-                                <p>Oopss... page not found</p>
-                            </main>
-                        }
-                    />
-                </Routes>
-            </AuthProvider>
+            <App/>
         </BrowserRouter>
     </React.StrictMode>
 );
